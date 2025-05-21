6d00f6506cc5a9393a9e907fd5605bcf6e13b9a6
Kishore2290 <iamrokishorekumar@gmail.com>
2025-05-21 11:57:32 +0530

    component name changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Courseview from './Components/Courseview'
+import CourseList from './Components/CourseList'
 import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Courseview />
+    <CourseList />
   </StrictMode>
 )
